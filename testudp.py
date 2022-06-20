f3d0476c8622c7d393f8d8f6ef4fde2a44c1ea96
Runmaru <mindek30@gmail.com>
2022-06-21 00:48:33 +0700

    add function test

---
+++ testudp.py
@@ -0,0 +1,13 @@
+import socket
+TCP_IP = "27.254.115.167"
+TCP_PORT = 1333
+MESSAGE = b"Check Haha"
+
+print("UDP target IP: %s" % TCP_IP)
+print("UDP target port: %s" % TCP_PORT)
+print("message: %s" % MESSAGE)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # TCP
+#sock.connect((TCP_IP, TCP_PORT))
+sock.sendto(MESSAGE,(TCP_IP, TCP_PORT))
